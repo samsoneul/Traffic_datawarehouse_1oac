b207726d28ccbbe0dbb46ee89065dfdb5ccb4375
samsoneul <samsonleul2@gmail.com>
2024-05-02 20:50:31 +0300

    models

---
+++ dbt/traffic/models/trajectory_summary.sql
@@ -0,0 +1,14 @@
+
+{{ config(materialized='table') }}
+WITH trajectory_summary AS(
+    SELECT
+        track_id,
+        MAX(speed) AS max_speed,
+        MAX (lat_acc) AS max_lat_acc,
+        MAX(lon_acc) AS max_lon_acc,
+        MAX(time) AS total_time
+    FROM "Trajectory_Data"
+    GROUP BY track_id
+)
+
+SELECT * FROM trajectory_summary
